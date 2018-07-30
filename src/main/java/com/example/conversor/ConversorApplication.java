c01c810bd0fc9a426cb6178a536ebd6f1b095686
Bruno Morais <bruno.nobrega@prf.gov.br>
2018-07-30 20:51:54 -0300

    First commit

---
+++ src/main/java/com/example/conversor/ConversorApplication.java
@@ -0,0 +1,12 @@
+package com.example.conversor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversorApplication.class, args);
+    }
+}
